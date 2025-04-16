684d0691cce34663930b767e2e58f9f4e7dc392d
Jas-Sidhu23 <112530672+Jas-Sidhu23@users.noreply.github.com>
2025-04-16 14:30:27 -0400

    Update lab_exam_practice.s

--- lab_exam_practice.s
+++ lab_exam_practice.s
@@ -4,14 +4,17 @@
 	.global mydata
 
 prompt:	.string "Your prompt with instructions is place here", 0
-mydata:	.byte	0x20	; This is where you can store data. 
+count: .word 	0x30
+names: .string "Jas and Arianna", 0
+
+mydata:	.byte	0x20	; This is where you can store data.
 			; The .byte assembler directive stores a byte
-			; (initialized to 0x20 in this case) at the label  
-			; mydata.  Halfwords & Words can be stored using the 
-			; directives .half & .word 
+			; (initialized to 0x20 in this case) at the label
+			; mydata.  Halfwords & Words can be stored using the
+			; directives .half & .word
 
 	.text
-	
+
 	.global uart_interrupt_init
 	.global gpio_interrupt_init
 	.global UART0_Handler
@@ -23,12 +26,14 @@ mydata:	.byte	0x20	; This is where you can store data.
 	.global output_string		; This is from your Lab #4 Library
 	.global uart_init		; This is from your Lab #4 Library
 	.global lab5
-	
+
 ptr_to_prompt:		.word prompt
 ptr_to_mydata:		.word mydata
+ptr_to_count:		.word count
+ptr_to_names:		.word names
 
-lab5:				; This is your main routine which is called from 
-				; your C wrapper.  
+lab5:				; This is your main routine which is called from
+				; your C wrapper.
 	PUSH {r4-r12,lr}   	; Preserve registers to adhere to the AAPCS
 	ldr r4, ptr_to_prompt
 	ldr r5, ptr_to_mydata
@@ -37,10 +42,15 @@ lab5:				; This is your main routine which is called from
 	bl uart_interrupt_init
 	bl gpio_interrupt_init
 
-	; This is where you should implement a loop, waiting for the user to 
+
+
+Infin:
+	B Infin
+
+	; This is where you should implement a loop, waiting for the user to
 	; indicate if they want to end the program.
- 
-	POP {lr}		; Restore registers to adhere to the AAPCS
+
+	POP {r4-r12,lr}		; Restore registers to adhere to the AAPCS
 	MOV pc, lr
 
 
@@ -162,32 +172,94 @@ gpio_interrupt_init:
 	  MOV pc, lr
 
 
-UART0_Handler: 
-	
+UART0_Handler:
+
 	; Your code for your UART handler goes here.
-	; Remember to preserver registers r4-r12 by pushing then popping 
+	; Remember to preserver registers r4-r12 by pushing then popping
 	; them to & from the stack at the beginning & end of the handler
+	PUSH {r4-r12,lr}   	; Preserve registers to adhere to the AAPCS
+
+	 ;Clear the interrupt
+    MOV r2, #0xC000
+    MOVT r2, #0x4000
+    LDR r3, [r2, #0x044]
+
+    ORR r3, #0x10
+
+    STR r3, [r2, #0x044]
+
+    ;get user inpuit
+    bl simple_read_character ;input char stored in r0 now
+
+    cmp r0, #'g'
+    bne EndUartHandle
+
+    MOV r0, #0xC				;clear the screen
+    bl output_character
+
+    ldr r0, ptr_to_names
+
+    bl output_string
+
+EndUartHandle:
+
+
+	POP {r4-r12,lr}		; Restore registers to adhere to the AAPCS
 
 	BX lr       	; Return
 
 
 Switch_Handler:
-	
+
 	; Your code for your UART handler goes here.
-	; Remember to preserver registers r4-r12 by pushing then popping 
+	; Remember to preserver registers r4-r12 by pushing then popping
 	; them to & from the stack at the beginning & end of the handler
+	PUSH {r4-r12,lr}   	; Preserve registers to adhere to the AAPCS
+
+	MOV r2, #0x5000				;clear the interrupt
+    MOVT r2, #0x4002
+    LDR r3, [r2, #0x041C]
+
+    ORR r3, #0x10
+
+    STR r3, [r2, #0x041C]
+
+    ldr r4, ptr_to_count		;load the counter
+    ldr r5, [r4]
+
+    ADD r5, r5, #1				;increment the counter
+
+    str r5, [r4]				; store the count back into address
+
+    CMP r5, #0x37
+    BGE illum
+
+must:
+    MOV r0, #0xC				;clear the screen
+    bl output_character
+
+    MOV r0, r5					;print the count
+    bl output_character
+    B Done
+
+illum:
+	bl illuminate_RGB_prac
+	B must
+
+Done:
+	POP {r4-r12,lr}		; Restore registers to adhere to the AAPCS
 
 	BX lr       	; Return
 
 
 Timer_Handler:
-	
+
 	; Your code for your Timer handler goes here.  It is not needed for
 	; Lab #5, but will be used in Lab #6.  It is referenced here because
 	; the interrupt enabled startup code has declared Timer_Handler.
-	; This will allow you to not have to redownload startup code for 
+	; This will allow you to not have to redownload startup code for
 	; Lab #6.  Instead, you can use the same startup code as for Lab #5.
-	; Remember to preserver registers r4-r12 by pushing then popping 
+	; Remember to preserver registers r4-r12 by pushing then popping
 	; them to & from the stack at the beginning & end of the handler.
 
 	BX lr       	; Return
@@ -200,6 +272,25 @@ simple_read_character:
   MOVT r4, #0x4000
   LDRB r5, [r4, #0x18] ;Load from memory
   LDRB r0, [r4]       ;Store in r0
+
+	POP {r4-r12,lr}   ; Restore registers from stack
+
+	MOV pc, lr
+
+illuminate_RGB_prac:
+	PUSH {r4-r12,lr} ; Spill registers to stack
+
+	;r1- address bucket
+    ;r2- regsiter data bucket
+
+	;Get Register which controls the light
+    MOV r1, #0x5000
+    MOVT r1, #0x4002 ; base address for GPIO Port F
+    LDR r2, [r1, #0x3FC]    ;Puts the data from reg into r2
+
+    ORR r2, r2, #0x2        ;set Pin 1
+
+    STR r2, [r1, #0x3FC]    ;Puts the data BACK with Appropriate color
 
 	POP {r4-r12,lr}   ; Restore registers from stack
 
